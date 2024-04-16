b97fd14c1d710cdefc56a4fe68094e51b55e3a13
雷迪亚兹 <1626467682@qq.com>
2024-04-16 22:02:35 +0800

    echarts

--- src/components/Supplier/static.vue
+++ src/components/Supplier/static.vue
@@ -104,88 +104,59 @@ export default {
       // 使用刚指定的配置项和数据显示图表
       this.chartLine.setOption(option);
     },
-    // async getChartList() {
-    //   const data1 = JSON.parse(window.sessionStorage.getItem("data"));
-    //   const { data: res } = await this.$http.get('getChartList?id=' + data1.data[0].id);
-    //   let str = this.$moment(this.date).format("YYYY-MM");
-    //   //x坐标
-    //   this.xlist[0] = str + "-06";
-    //   this.xlist[1] = str + "-11";
-    //   this.xlist[2] = str + "-16";
-    //   this.xlist[3] = str + "-21";
-    //   this.xlist[4] = str + "-26";
-    //   this.xlist[5] = str + "-";
-    //   //y坐标
+    async getChartList() {
+      const data1 = JSON.parse(window.sessionStorage.getItem("data"));
+      const { data: res } = await this.$http.get('getChartList?id=' + data1.data[0].id);
+      let str = this.$moment(this.date).format("YYYY-MM");
+      //x坐标
+      this.xlist[0] = str + "-06";
+      this.xlist[1] = str + "-11";
+      this.xlist[2] = str + "-16";
+      this.xlist[3] = str + "-21";
+      this.xlist[4] = str + "-26";
+      this.xlist[5] = str + "-";
+      //y坐标
 
-    //   //钱和时间
-    //   if (res.success) {
-    //     //时间，交易金额
-    //     this.chartList = res.date;
-    //     // console.log('this.chartList');
-    //     // console.log(this.chartList);
-    //     for (let i = 0; i < 7; i++) {
-    //       this.ylist1[i] = 0;
-    //       this.ylist2[i] = 0;
-    //     }
-    //     for (let i = 0; i < this.chartList.length; i++) {
-    //       //在这个月的
-    //       if (((this.chartList[i].date - 30 * 24 * 60 * 60 * 1000) < (this.date)) && (
-    //         this.chartList[i].date > (this.date)
-    //       )) {
-    //         if ((this.chartList[i].date - 5 * 24 * 60 * 60 * 1000 < this.date)) {
-    //           this.ylist1[0]++;
-    //           this.ylist2[0] += this.chartList[i].reality_price;
-    //         } else if ((this.chartList[i].date - 10 * 24 * 60 * 60 * 1000 < this.date)) {
-    //           this.ylist1[1]++;
-    //           this.ylist2[1] += this.chartList[i].reality_price;
-    //         } else if ((this.chartList[i].date - 15 * 24 * 60 * 60 * 1000 < this.date)) {
-    //           this.ylist1[2]++;
-    //           this.ylist2[2] += this.chartList[i].reality_price;
-    //         } else if ((this.chartList[i].date - 20 * 24 * 60 * 60 * 1000 < this.date)) {
-    //           this.ylist1[3]++;
-    //           this.ylist2[3] += this.chartList[i].reality_price;
-    //         } else if ((this.chartList[i].date - 25 * 24 * 60 * 60 * 1000 < this.date)) {
-    //           this.ylist1[4]++;
-    //           this.ylist2[4] += this.chartList[i].reality_price;
-    //         } else {
-    //           this.ylist1[5]++;
-    //           this.ylist2[5] += this.chartList[i].reality_price;
-    //         }
-    //       }
-    //     }
-    //   }
-    //   this.drawLineChart();
-    // },
-    // async getChartList() {
-    //   const data1 = JSON.parse(window.sessionStorage.getItem("data"));
-    //   const { data: res } = await this.$http.get('getChartList?id=' + data1.data[0].id);
-    //   let str = this.$moment(this.date).format("YYYY-MM");
+      //钱和时间
+      if (res.success) {
+        //时间，交易金额
+        this.chartList = res.date;
+        // console.log('this.chartList');
+        // console.log(this.chartList);
+        for (let i = 0; i < 7; i++) {
+          this.ylist1[i] = 0;
+          this.ylist2[i] = 0;
+        }
+        for (let i = 0; i < this.chartList.length; i++) {
+          //在这个月的
+          if (((this.chartList[i].date - 30 * 24 * 60 * 60 * 1000) < (this.date)) && (
+            this.chartList[i].date > (this.date)
+          )) {
+            if ((this.chartList[i].date - 5 * 24 * 60 * 60 * 1000 < this.date)) {
+              this.ylist1[0]++;
+              this.ylist2[0] += this.chartList[i].reality_price;
+            } else if ((this.chartList[i].date - 10 * 24 * 60 * 60 * 1000 < this.date)) {
+              this.ylist1[1]++;
+              this.ylist2[1] += this.chartList[i].reality_price;
+            } else if ((this.chartList[i].date - 15 * 24 * 60 * 60 * 1000 < this.date)) {
+              this.ylist1[2]++;
+              this.ylist2[2] += this.chartList[i].reality_price;
+            } else if ((this.chartList[i].date - 20 * 24 * 60 * 60 * 1000 < this.date)) {
+              this.ylist1[3]++;
+              this.ylist2[3] += this.chartList[i].reality_price;
+            } else if ((this.chartList[i].date - 25 * 24 * 60 * 60 * 1000 < this.date)) {
+              this.ylist1[4]++;
+              this.ylist2[4] += this.chartList[i].reality_price;
+            } else {
+              this.ylist1[5]++;
+              this.ylist2[5] += this.chartList[i].reality_price;
+            }
+          }
+        }
+      }
+      this.drawLineChart();
+    },
 
-    //   this.xlist = []; // 在添加新值之前清除xlist
-    //   this.ylist1 = new Array(6).fill(0); // 使用0初始化ylist1
-    //   this.ylist2 = new Array(6).fill(0); // 使用0初始化ylist2
-
-    //   // 用日期填充xlist
-    //   for (let i = 1; i <= 6; i++) {
-    //     this.xlist.push(str + "-" + (i * 5).toString().padStart(2, '0'));
-    //   }
-
-    //   if (res.success) {
-    //     this.chartList = res.date;
-
-    //     for (let i = 0; i < this.chartList.length; i++) {
-    //       const chartDate = new Date(this.chartList[i].date);
-    //       const daysDiff = Math.floor((new Date() - chartDate) / (1000 * 60 * 60 * 24)); // 计算日期差
-
-    //       if (daysDiff < 30) {
-    //         const index = Math.floor(daysDiff / 5); // 根据日期确定索引
-    //         this.ylist1[index]++;
-    //         this.ylist2[index] += this.chartList[i].reality_price;
-    //       }
-    //     }
-    //   }
-    //   this.drawLineChart();
-    // },
     async getChartList1() {
       const data1 = JSON.parse(window.sessionStorage.getItem("data"));
 
